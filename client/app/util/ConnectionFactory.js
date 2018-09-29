fd571d21935dac7e91d37beba0d1955cabe1eec5
josimarjunior <juniorpaz99@gmail.com>
2018-09-29 15:17:28 -0300

    Creating the ConnectionFactory

---
+++ client/app/util/ConnectionFactory.js
@@ -0,0 +1,56 @@
+const ConnectionFactory = (() => {
+
+    const stores = ['negotiations'];
+    let connection = null;
+    let close = null;
+
+    return class ConnectionFactory {
+
+        constructor() {
+            throw new Error("Can't create instances of this class");
+        }
+
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+
+                if (connection)
+                    return resolve(connection);
+
+                const openRequest = indexedDB.open('js', 2);
+
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result);
+                }
+
+                openRequest.onsuccess = e => {
+                    connection = e.target.result;
+                    close = connection.close.bind(connection);
+                    connection.close = () => {
+                        throw new Error("You can't close the connection directly");
+                    }
+                    resolve(connection);
+                }
+
+                openRequest.onerror = e => {
+                    console.log(e.target.error);
+                    reject(e.target.error.name);
+                }
+            });
+        }
+
+        static _createStores(connection) {
+            stores.forEach(store => {
+                if (connection.objectStoreNames.contains(store))
+                    connection.deleteObjectStore(store);
+
+                connection.createObjectStore(store, { autoIncrement: true });
+            })
+        }
+
+        static closeConnection() {
+            if (connection)
+                close();
+        }
+    }
+
+})();
