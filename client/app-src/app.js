ca81740e7cf7f59569c578a8c627b801c1a39733
josimarjunior <juniorpaz99@gmail.com>
2018-10-03 21:39:44 -0300

    Importing scripts

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,10 +1,17 @@
 import	'bootstrap/dist/css/bootstrap.css';
 import	'bootstrap/dist/css/bootstrap-theme.css';
 
+import	'bootstrap/js/modal.js';
+
 import '../css/mycss.css';
 
 import { NegotiationController } from './controllers/NegotiationController.js';
 import { Negotiation } from './domain/index.js';
+
+$('h1').on('click', () => alert('Was clicked'));
+
+console.log('Function added by Bootstrap:');
+console.log($('h1').modal);
 
 const negotiation = new Negotiation(new Date(), 10, 30);
 const headers = new Headers();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,11 +3,26 @@ const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
+const webpack = require('webpack');
+
 let plugins = [];
 
 plugins.push(new extractTextPlugin("styles.css"));
 
+plugins.push(new webpack.ProvidePlugin({
+    $: 'jquery/dist/jquery.js',
+    jQuery: 'jquery/dist/jquery.js'
+}));
+
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor.bundle.js'
+}));
+
 if (process.env.NODE_ENV == 'production') {
+
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
+
     plugins.push(new babiliPlugin());
 
     plugins.push(new optimizeCSSAssetsPlugin({
@@ -22,7 +37,10 @@ if (process.env.NODE_ENV == 'production') {
 }
 
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
